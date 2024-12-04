9dcb2a0fd879977e3510569d683c836fe67d099b
Ivan Maulana <153053319+vanzy505@users.noreply.github.com>
2024-12-04 19:17:51 +0700

    Update MatematikaVcanggih.java

--- MatematikaVcanggih.java
+++ MatematikaVcanggih.java
@@ -9,20 +9,26 @@ package matematikav;
  * @author iv4nm
  */
 public class MatematikaVcanggih extends MatematikaV {
-   void modulus(double a ,double b , double c){
-        double hasil = a % b % c;
-        System.out.println("Hasil Modulus: " + hasil);
+      //  overload modulus 
+    void modulus(double a ,double b , double c){
+        hasilDouble = a % b % c;
+        System.out.println("Hasil Modulus: " + hasilDouble);
     }
     void modulus(int a ,int b , int c){
-        int hasil = a % b % c;
-        System.out.println("Hasil Modulus: " + hasil);
+        hasilInt = a % b % c;
+        System.out.println("Hasil Modulus: " + hasilInt);
     }
     void modulus(int a ,int b){
-        int hasil = a % b;
-        System.out.println("Hasil Modulus: " + hasil);
+        hasilInt = a % b;
+        System.out.println("Hasil Modulus: " + hasilInt);
     }
     void modulus(double a ,double b){
-        double hasil = a % b ;
-        System.out.println("Hasil Modulus: " + hasil);
+        hasilDouble = a % b ;
+        System.out.println("Hasil Modulus: " + hasilDouble);
+    }
+
+    // Method operasiGabungan
+    int operasiGabungan(int a , int b , int c){
+        return a + (b * c);
     }
 }
